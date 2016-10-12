447a9b4423616dadf230e4fe19eb60f29fcff0b4
Aishwarya <aishwarya.borkar@sjsu.edu>
2016-10-12 11:31:33 -0700

    iterate through array of JSON objects and render them

--- public/js/controllers/ForumController.js
+++ public/js/controllers/ForumController.js
@@ -2,4 +2,44 @@ angular.module('ForumController', []).controller('ForumController', function($sc
 
     $scope.heading = "How do I leverage my network?";
 
+    var posts = 
+
+    	[{
+    		postName: "LinkedIn or LinkedOut?", 
+    		postAuthor: "Stacy Wong", 
+    		postDate: "July 27, 2016", 
+    		postContent:"Default content"
+    	}, 
+
+    	{
+    		postName: "MEAN Stack, Play Nice", 
+    		postAuthor: "Jonathan Chen", 
+    		postDate: "August 18, 2016", 
+    		postContent:"Default content"
+
+    	}, 
+
+    	{
+    		postName: "My Reaction to React.js", 
+    		postAuthor: "Aishwarya Borkar", 
+    		postDate: "October 5, 2016", 
+    		postContent:"Default content"
+
+    	}, 
+
+    	{
+    		postName: "Big Data, Big Problems", 
+    		postAuthor: "Anna Meng", 
+    		postDate: "October 12, 2016", 
+    		postContent:"Default content"
+
+    	}]; 
+    
+
+    $scope.posts = posts; 
+
+
+
+
+
 });
