7cc103bc1a60bce754ef77a73976d9214e263263
joshsylvia <josh.sylvia@sjsu.edu>
2016-10-12 12:42:10 -0700

    Updated search page

--- public/js/controllers/SearchController.js
+++ public/js/controllers/SearchController.js
@@ -7,15 +7,10 @@ angular.module('SearchController', []).controller('SearchController', function($
 			company:"net[work]"
 		},
 		{
-			name:"Jonathan Chen",
-			jobTitle:"Software Consultant",
-			Company:"Thomson Reuters"
-		},
-		{
 			name:"Ash Borkar",
 			jobTitle:"Software Developer",
 			Company:"Visa"
-		}]
+		}];
 
-	
+	$scope.results = queryResult;
 });
