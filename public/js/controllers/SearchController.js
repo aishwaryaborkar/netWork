30769e3b7dc82876f84705646034924474a3f882
joshsylvia <josh.sylvia@sjsu.edu>
2016-10-27 13:09:16 -0700

    Search with all fields. Still needs to show/hide fields between user and premium user.

--- public/js/controllers/SearchController.js
+++ public/js/controllers/SearchController.js
@@ -5,6 +5,7 @@ angular.module('SearchController', ['DataService']).controller('SearchController
 	var isPremuiumUser = false;
 	$scope.premiumUser = isPremiumUser;
 	
+	
 	$scope.results = [];
 	
     $scope.handleSubmit = function() {
