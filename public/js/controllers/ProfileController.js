d546fe5de6db54de4343a8920ffea95b1c26c5c9
navjotbola <navjotb1@gmail.com>
2016-11-06 11:08:51 -0800

    testing getProfileById

--- public/js/controllers/ProfileController.js
+++ public/js/controllers/ProfileController.js
@@ -1,9 +1,30 @@
-angular.module('ProfileController', []).controller('ProfileController', function($scope) {
+angular.module('ProfileController', ['DataService']).controller('ProfileController', function($scope, $rootScope, dataService) {
     console.log('ProfileController loaded[]');
 	toastr.success('Welcome to netWork!');
 
-
+	$scope.modalHeader = ''
+	console.log($rootScope.userId);
 	$scope.addButton = function(){
 		// window.location.href = "/forum";
-	}
+	};
+
+	$scope.summaryClick = function(){
+		$scope.modalHeader = 'Summary'
+	};
+
+	$scope.eduClick = function(){
+		$scope.modalHeader = 'Education'
+	};
+
+	$scope.expClick = function(){
+		$scope.modalHeader = 'Experience'
+	};
+
+	$scope.skillsClick = function(){
+		$scope.modalHeader = 'Skills'
+	};
+
+	dataService.getProfile({'userId': $rootScope.userId}).then(function(data){
+		console.log(data);
+	});
 });
