f91d23c2095b911b7ce125e7c967b1e227d3affc
joshsylvia <josh.sylvia@sjsu.edu>
2016-11-13 17:11:28 -0800

    Fixed the funky merge search.

--- public/js/controllers/SearchController.js
+++ public/js/controllers/SearchController.js
@@ -6,19 +6,19 @@ angular.module('SearchController', ['DataService']).controller('SearchController
     $scope.company = '';
     $scope.jobtitle = '';
     $scope.skill = '';
-    $scope.endorsement = '';
     $scope.schoolName = '';
     $scope.graduationDate = '';
     $scope.description = '';
     $scope.mysearch = '';
 
+
     $scope.clearFields = function() {
+    	$scope.results = [];
     	$scope.email = '';
         $scope.name = '';
         $scope.company = '';
         $scope.jobtitle = '';
         $scope.skill = '';
-        $scope.endorsement = '';
     	$scope.schoolName = '';
    		$scope.graduationDate = '';
     	$scope.description = '';
@@ -33,16 +33,27 @@ angular.module('SearchController', ['DataService']).controller('SearchController
 		return true;
 	}
 	
-
+	$scope.skillSearch = [];  // creates new array for skills
+	$scope.searchSkills = function() {
+	//	skillSearch = skill.split(",")     // to split skills into array
+	//	for (a in skillSearch) {
+	//		skillSearch[a] = skillSearch[a].trim();  // to trim any whitespace
+  }
 	
     $scope.handleSubmit = function() {
 		//compile info into an searchObject for api
+	
+	//	searchSkills();
+	
 		var searchCriteria = {};
 		searchCriteria.email = $scope.email;
 		searchCriteria.name = $scope.name;
 		searchCriteria.company = $scope.company;
 		searchCriteria.jobTitle = $scope.jobtitle;
+		
 		searchCriteria.skill = $scope.skill;
+  // searchCriteria.skill = $scope.skillSearch;  // search by array.
+		
 		searchCriteria.endorsement = $scope.endorsement;
 		searchCriteria.education = $scope.education;
 		
